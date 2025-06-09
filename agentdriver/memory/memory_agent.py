2eb06e8a7c348bd1095e20ebd31c91260cdbc6e2
Anarghya H <47393170+anarghya15@users.noreply.github.com>
2025-06-09 14:49:04 +0530

    Update memory_agent.py

--- agentdriver/memory/memory_agent.py
+++ agentdriver/memory/memory_agent.py
@@ -5,11 +5,12 @@ from agentdriver.memory.common_sense_memory import CommonSenseMemory
 from agentdriver.memory.experience_memory import ExperienceMemory
 
 class MemoryAgent:
-    def __init__(self, data_path, model_name="gpt-3.5-turbo-0613", verbose=False, compare_perception=False) -> None:
+    def __init__(self, data_path, model_name="gpt-3.5-turbo-0613", verbose=False, compare_perception=False, backend="openai") -> None:
         self.model_name = model_name
-        self.common_sense_memory = CommonSenseMemory()
-        self.experience_memory = ExperienceMemory(data_path, model_name=self.model_name, verbose=verbose, compare_perception=compare_perception)
+        self.common_sense_memory = CommonSenseMemory()        
         self.verbose = verbose
+        self.backend = backend
+        self.experience_memory = ExperienceMemory(data_path, model_name=self.model_name, verbose=verbose, compare_perception=compare_perception, backend=self.backend)
 
     def retrieve(self, working_memory):
         raise NotImplementedError
@@ -26,7 +27,7 @@ class MemoryAgent:
     def update(self, working_memory):
         raise NotImplementedError
 
-    @timeout(15)
+    @timeout(60)
     def run(self, working_memory):
         common_sense_prompts = self.retrieve_common_sense_memory()
         experience_prompt = self.retrieve_experience_memory(working_memory)

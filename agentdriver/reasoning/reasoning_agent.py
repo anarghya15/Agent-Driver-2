faaceb551c4d5d24bbdfdc1d54ecdaa6db91be0d
Anarghya H <47393170+anarghya15@users.noreply.github.com>
2025-06-09 15:05:01 +0530

    Update reasoning_agent.py

--- agentdriver/reasoning/reasoning_agent.py
+++ agentdriver/reasoning/reasoning_agent.py
@@ -10,9 +10,10 @@ from agentdriver.reasoning.prompt_reasoning import (
 )
 
 class ReasoningAgent:
-    def __init__(self, model_name="gpt-3.5-turbo-0613", verbose=True) -> None:
+    def __init__(self, model_name="gpt-3.5-turbo-0613", verbose=True, backend="openai") -> None:
         self.verbose = verbose
         self.model_name = model_name
+        self.backend=backend
 
     def generate_chain_of_thoughts_target(self, data_dict, working_memory):
         """Generating reasoning targets by rules, can be used as fine-tuning"""
@@ -21,15 +22,15 @@ class ReasoningAgent:
             print(reasoning)
         return reasoning
 
-    @timeout(15)
+    @timeout(60)
     def generate_chain_of_thoughts_reasoning(self, env_info_prompts):
         """Generating chain_of_thoughts reasoning by GPT in-context learning"""
-        reasoning = generate_reasoning_results(env_info_prompts, self.model_name)
+        reasoning = generate_reasoning_results(env_info_prompts, self.model_name, self.backend)
         if self.verbose:
             print(reasoning)
         return reasoning
     
-    @timeout(15)
+    @timeout(60)
     def run(self, data_dict, env_info_prompts, working_memory, use_cot_rules=False):
         """Generate planning target and chain_of_thoughts reasoning"""
         if use_cot_rules:
